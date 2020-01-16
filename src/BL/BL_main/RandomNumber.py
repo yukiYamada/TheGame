331d9546356f49b6db20bb285b179d238f58a6ec
yuki yamada <couraeg0425@gmail.com>
2020-01-16 15:16:24 +0000

    update number instance

--- src/BL/BL_main/RandomNumber.py
+++ src/BL/BL_main/RandomNumber.py
@@ -1,2 +1,28 @@
-class RandumNumber:
-    None
+class Number:
+    '''
+    Number Class.
+    '''
+    def create(number):
+        '''
+        Number Class Basic Factory.
+        '''
+        if (number > 99) :
+            raise InvalidArgumentExceptionOfNumber("over number")
+        if (number < 2):
+            raise InvalidArgumentExceptionOfNumber("under number")
+        return Number(number)   
+    def __init__(self, number):
+        '''
+        Constractor.
+        '''
+        self.value = number
+    def __eq__(self, other):
+        '''
+        Override Equals.
+        '''
+        if not isinstance(other, Number):
+            return NotImplemented
+        return self.value == other.value
+
+class InvalidArgumentExceptionOfNumber(Exception):
+    pass
--- src/BL/test/test_RandomNumber.py
+++ src/BL/test/test_RandomNumber.py
@@ -1,5 +1,32 @@
-from BL_main import RandomNumber 
+#third
+import pytest
+#user
+from BL_main.RandomNumber import *
 
-def test_rundam_number():
-    None
+def test_NumberClass_InvalidException_underNumber():
+    '''
+    Test argument. under number.
+    '''
+    with pytest.raises(InvalidArgumentExceptionOfNumber):
+        Number.create(1)
+    Number.create(2)
+def test_NumberClass_InvalidException_overNumber():
+    '''
+    Test argument. over number.
+    '''
+    with pytest.raises(InvalidArgumentExceptionOfNumber):
+        Number.create(100)
+    Number.create(99)
+def test_NumberClass_CreateTargetNumber():
+    '''
+    Test NumberClass initialize. 
+    '''
+    actual = Number.create(3)
+    assert actual.value == 3
+    
+    actual2 = Number.create(3)
+    assert actual == actual2
+
+    actual3 = Number.create(4)
+    assert actual != actual3
 
